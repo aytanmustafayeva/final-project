91eba81c32c916a32c96cf05953c25a29965f2f8
aytanmustafayeva <105521095+aytanmustafayeva@users.noreply.github.com>
2022-09-22 19:01:45 +0400

    Add files via upload

---
+++ src/components/Footerdetail/Footerdetail.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Footerdetail.css";
+
+function Footerdetail() {
+  return (
+    <div>
+      <div class="list-group">
+        <a type="button" class="list-group-item list-group-item-action">
+          About Us
+        </a>
+        <a type="button" class="list-group-item list-group-item-action">
+          Contact
+        </a>
+        <a type="button" class="list-group-item list-group-item-action">
+          FAQ
+        </a>
+        <a type="button" class="list-group-item list-group-item-action">
+          Support
+        </a>
+        <a type="button" class="list-group-item list-group-item-action">
+          Terms and Contidions
+        </a>
+        <a type="button" class="list-group-item list-group-item-action">
+          Refund and exchange
+        </a>
+        <a type="button" class="list-group-item list-group-item-action">
+          Privacy Policy
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Footerdetail;
---
+++ src/pages/FAQ/FAQ.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./FAQ.css";
+
+function FAQ() {
+  return <div></div>;
+}
+
+export default FAQ;
